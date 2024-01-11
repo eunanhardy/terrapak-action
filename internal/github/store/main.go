552a3fd8f0878d56d4c83431a17b3771baa7245e
Eunan Hardy <ehardy2000@googlemail.com>
2024-01-11 23:32:14 +0000

    Update main.go

--- internal/github/store/main.go
+++ internal/github/store/main.go
@@ -27,9 +27,9 @@ func Print() string {
 	var output string
 	for idx, rs := range resultSet {
 		if idx == len(resultSet)-1 {
-			output += fmt.Sprintf("| %s | %s | %s |", rs.Name, rs.Version, rs.Change)
+			output += fmt.Sprintf(" | %s | %s | %s |", rs.Name, rs.Version, rs.Change)
 		} else {
-			output += fmt.Sprintf("| %s | %s | %s | <br>", rs.Name, rs.Version, rs.Change)
+			output += fmt.Sprintf(" | %s | %s | %s | <br>", rs.Name, rs.Version, rs.Change)
 		}
 	}
 	return output
