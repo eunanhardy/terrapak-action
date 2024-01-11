1a09f2ac3750494042b0393eb52e8103dee5c50b
Eunan Hardy <ehardy2000@googlemail.com>
2024-01-11 23:21:30 +0000

    Update github.go

--- internal/github/github.go
+++ internal/github/github.go
@@ -14,7 +14,7 @@ import (
 const TABLE_TEMPLATE = `## Terrapak Sync
 Changes detected in the following modules.
 | Module | Version | Action |
-| :---: | :---: | :---: | <br>
+| :---: | :---: | :---: |
 `
 
 func AddPRComment(markdown string) {
